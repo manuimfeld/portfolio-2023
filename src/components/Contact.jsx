879df2c6fbbbab89afabf9d331d23dce6f1b2ea1
Manuimfeld <manu_jadir@hotmail.com>
2023-07-09 13:18:22 -0300

    Edit and add contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,11 +2,67 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="bg-[#202833] mx-auto h-[calc(100vh_-_60px)] lg:px-[40px]"
+      className="bg-[#202833] mx-auto min-h-[calc(100vh_-_60px)] lg:px-[40px] w-[100%] p-[20px]"
     >
-      <h2 className="text-color-primary mx-auto w-fit text-2xl pt-10">
+      <h2 className="text-color-primary mx-auto w-fit text-2xl mb-5">
         Contacto
       </h2>
+
+      <form
+        id="fs-frm"
+        name="simple-contact-form"
+        acceptCharset="utf-8"
+        action={""}
+        method="post"
+        className="bg-color-bg-third shadow-md rounded-[15px] px-8 pt-6 pb-8 mb-4 w-[100%] sm:w-[60%] lg:w-[50%] mx-auto"
+      >
+        <div className="mb-4">
+          <label
+            className="block text-text-primary text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Nombre
+          </label>
+          <input
+            className="bg-color-bg-secondary shadow appearance-none border border-color-secondary rounded w-full py-2 px-3 text-text-primary leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="name"
+            id="full-name"
+            placeholder="Ej: Gustavo Lopez"
+            required=""
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-text-primary text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Email
+          </label>
+          <input
+            className="bg-color-bg-secondary shadow appearance-none border  border-color-secondary rounded w-full py-2 px-3 text-text-primary  mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="email"
+            name="_replyto"
+            id="email-address"
+            placeholder="Ej: loremips@gmail.com"
+            required
+          />
+        </div>
+        <textarea
+          rows="5"
+          name="message"
+          id="message"
+          placeholder="Escribe tu mensaje aquí."
+          required=""
+          className="bg-color-bg-secondary shadow appearance-none border  border-color-secondary rounded w-full py-2 px-3 text-text-primary  mb-3 leading-tight focus:outline-none focus:shadow-outline"
+        />
+        <button
+          className="w-[50%] ml-[25%] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+        >
+          Enviar mensaje
+        </button>
+      </form>
     </section>
   );
 };
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,12 +1,161 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
 const Works = () => {
+  const [showDescription, setShowDescription] = useState("");
+
+  const handleTap = (work) => {
+    if (showDescription !== work) {
+      setShowDescription(work);
+    } else {
+      setShowDescription("");
+    }
+  };
+
   return (
     <section
       id="works"
-      className="bg-[#202833] mx-auto h-[calc(100vh_-_60px)] lg:px-[40px]"
+      className="text-center bg-[#202833] flex flex-col sm:content-start sm:flex-row sm:flex-wrap lg:justify-around justify-start items-start mx-auto min-h-[calc(100vh_-_60px)] px-[20px] lg:px-[40px]"
     >
-      <h2 className="text-color-primary mx-auto w-fit text-2xl pt-10">
-        Habilidades
+      <h2 className="text-color-primary mx-auto w-fit sm:w-full h-fit mb-5 text-2xl pt-10">
+        Proyectos
       </h2>
+      <motion.article
+        className="cursor-pointer relative h-[200px] lg:h-[300px] w-[90%] sm:w-[40%] lg:w-[30%] mx-auto lg:mx-0 my-5 rounded-[15px]"
+        whileHover={{ scale: 1.1 }}
+      >
+        <motion.img
+          src="https://manuimfeld.com.ar/img/trivia.png"
+          alt=""
+          className="w-[100%] h-full object-cover rounded-[10px] border-2 border-color-primary"
+        />
+        <motion.div
+          className="text-center py-2 px-4 absolute top-0 opacity-0 flex flex-col justify-between items-center bg-color-bg-third h-[200px] lg:h-[300px] w-[100%]  rounded-[10px] border-2 border-color-primary"
+          animate={{ opacity: showDescription === "trivia" ? 1 : 0 }}
+          onTap={() => handleTap("trivia")}
+        >
+          <h2 className="text-text-primary mt-[10px] lg:text-xl">
+            Trivia YSY-A
+          </h2>
+          <p className="text-xs text-text-secondary lg:text-lg">
+            Un quizz donde tenés que responder qué canción se está reproduciendo
+          </p>
+          <p className="text-text-primary mt-auto lg:text-xl">Tecnologias</p>
+          <p className="text-sm font-bold text-color-secondary lg:mb-auto lg:text-lg">
+            React React-router-dom HTML, CSS
+          </p>
+          <a
+            href="https://trivia-ysya.manuimfeld.com.ar/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-color-primary mt-auto lg:mt-0 mb-2 font-bold uppercase py-1 px-4 border-2 border-color-primary rounded-[20px]"
+          >
+            Ver proyecto
+          </a>
+        </motion.div>
+      </motion.article>
+      <motion.article
+        className="cursor-pointer relative h-[200px] lg:h-[300px] w-[90%] sm:w-[40%] lg:w-[30%] mx-auto lg:mx-0 my-5 rounded-[15px]"
+        whileHover={{ scale: 1.1 }}
+      >
+        <motion.img
+          src="https://manuimfeld.com.ar/img/trivia.png"
+          alt=""
+          className="w-[100%] h-full object-cover rounded-[10px] border-2 border-color-primary"
+        />
+        <motion.div
+          className="text-center py-2 px-4 absolute top-0 opacity-0 flex flex-col justify-between items-center bg-color-bg-third h-[200px] lg:h-[300px] w-[100%]  rounded-[10px] border-2 border-color-primary"
+          animate={{ opacity: showDescription === "trivia" ? 1 : 0 }}
+          onTap={() => handleTap("trivia")}
+        >
+          <h2 className="text-text-primary mt-[10px] lg:text-xl">
+            Trivia YSY-A
+          </h2>
+          <p className="text-xs text-text-secondary lg:text-lg">
+            Un quizz donde tenés que responder qué canción se está reproduciendo
+          </p>
+          <p className="text-text-primary mt-auto lg:text-xl">Tecnologias</p>
+          <p className="text-sm font-bold text-color-secondary lg:mb-auto lg:text-lg">
+            React React-router-dom HTML, CSS
+          </p>
+          <a
+            href="https://trivia-ysya.manuimfeld.com.ar/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-color-primary mt-auto lg:mt-0 mb-2 font-bold uppercase py-1 px-4 border-2 border-color-primary rounded-[20px]"
+          >
+            Ver proyecto
+          </a>
+        </motion.div>
+      </motion.article>
+      <motion.article
+        className="cursor-pointer relative h-[200px] lg:h-[300px] w-[90%] sm:w-[40%] lg:w-[30%] mx-auto lg:mx-0 my-5 rounded-[15px]"
+        whileHover={{ scale: 1.1 }}
+      >
+        <motion.img
+          src="https://manuimfeld.com.ar/img/trivia.png"
+          alt=""
+          className="w-[100%] h-full object-cover rounded-[10px] border-2 border-color-primary"
+        />
+        <motion.div
+          className="text-center py-2 px-4 absolute top-0 opacity-0 flex flex-col justify-between items-center bg-color-bg-third h-[200px] lg:h-[300px] w-[100%]  rounded-[10px] border-2 border-color-primary"
+          animate={{ opacity: showDescription === "trivia" ? 1 : 0 }}
+          onTap={() => handleTap("trivia")}
+        >
+          <h2 className="text-text-primary mt-[10px] lg:text-xl">
+            Trivia YSY-A
+          </h2>
+          <p className="text-xs text-text-secondary lg:text-lg">
+            Un quizz donde tenés que responder qué canción se está reproduciendo
+          </p>
+          <p className="text-text-primary mt-auto lg:text-xl">Tecnologias</p>
+          <p className="text-sm font-bold text-color-secondary lg:mb-auto lg:text-lg">
+            React React-router-dom HTML, CSS
+          </p>
+          <a
+            href="https://trivia-ysya.manuimfeld.com.ar/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-color-primary mt-auto lg:mt-0 mb-2 font-bold uppercase py-1 px-4 border-2 border-color-primary rounded-[20px]"
+          >
+            Ver proyecto
+          </a>
+        </motion.div>
+      </motion.article>
+      <motion.article
+        className="cursor-pointer relative h-[200px] lg:h-[300px] w-[90%] sm:w-[40%] lg:w-[30%] mx-auto lg:mx-0 my-5 rounded-[15px]"
+        whileHover={{ scale: 1.1 }}
+      >
+        <motion.img
+          src="https://manuimfeld.com.ar/img/trivia.png"
+          alt=""
+          className="w-[100%] h-full object-cover rounded-[10px] border-2 border-color-primary"
+        />
+        <motion.div
+          className="text-center py-2 px-4 absolute top-0 opacity-0 flex flex-col justify-between items-center bg-color-bg-third h-[200px] lg:h-[300px] w-[100%]  rounded-[10px] border-2 border-color-primary"
+          animate={{ opacity: showDescription === "trivia" ? 1 : 0 }}
+          onTap={() => handleTap("trivia")}
+        >
+          <h2 className="text-text-primary mt-[10px] lg:text-xl">
+            Trivia YSY-A
+          </h2>
+          <p className="text-xs text-text-secondary lg:text-lg">
+            Un quizz donde tenés que responder qué canción se está reproduciendo
+          </p>
+          <p className="text-text-primary mt-auto lg:text-xl">Tecnologias</p>
+          <p className="text-sm font-bold text-color-secondary lg:mb-auto lg:text-lg">
+            React React-router-dom HTML, CSS
+          </p>
+          <a
+            href="https://trivia-ysya.manuimfeld.com.ar/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-color-primary mt-auto lg:mt-0 mb-2 font-bold uppercase py-1 px-4 border-2 border-color-primary rounded-[20px]"
+          >
+            Ver proyecto
+          </a>
+        </motion.div>
+      </motion.article>
     </section>
   );
 };
