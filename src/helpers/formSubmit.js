98f65d29ff6806647151068261e728c620c45ec5
Manuimfeld <manu_jadir@hotmail.com>
2023-07-09 19:23:48 -0300

    function helper for form onsubmit

---
+++ src/helpers/formSubmit.js
@@ -0,0 +1,20 @@
+export const handleSubmit = (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  fetch(import.meta.env.VITE_FORM_URI, {
+    method: "POST",
+    body: formData,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then(() => {
+      alert("Mensaje enviado");
+      e.target.reset(); // Restablecer el formulario después de enviar
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
