d29db6613c17ac0c100a654a539c2fc331c2b5b2
Manuimfeld <manu_jadir@hotmail.com>
2023-07-09 19:24:12 -0300

    Edit path work component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Nav from "./components/Nav/Nav";
 import Skills from "./components/Skills";
-import Works from "./components/Works";
+import Works from "./components/Works/Works";
 
 function App() {
   const [activeNavItem, setActiveNavItem] = useState("home");
